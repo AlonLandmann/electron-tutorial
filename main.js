6dd692a9a6bdfff0321668dd861c7b3b0c392dab
AlonLandmann <rafael.landmann@gmail.com>
2025-02-16 14:32:06 +0800

    use a preload script to display versions

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-import { app, BrowserWindow } from "electron";
+const { app, BrowserWindow } = require("electron");
+const path = require("node:path");
 
 function createWindow() {
     const window = new BrowserWindow({
         width: 800,
         height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+        },
     });
 
     window.loadFile("index.html");
