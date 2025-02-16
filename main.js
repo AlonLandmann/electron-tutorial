d9667475c8150308babf69e78c26758a7f5a9035
AlonLandmann <rafael.landmann@gmail.com>
2025-02-16 13:53:44 +0800

    open new window on activation for macOS

--- main.js
+++ main.js
@@ -11,10 +11,16 @@ function createWindow() {
 
 app.whenReady().then(() => {
     createWindow();
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
 });
 
 app.on("window-all-closed", () => {
-    if (["win32", "linux"].includes(process.platform)) {
+    if (process.platform !== "darwin") {
         app.quit();
     }
 });
